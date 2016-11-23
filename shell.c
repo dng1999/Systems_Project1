cc7a1fa7a127e47cf7c572f3b9465321b8400d75
yureistar <dng1@stuy.edu>
2016-11-23 11:44:13 -0500

    ??

--- shell.c
+++ shell.c
@@ -3,46 +3,40 @@
 #include <unistd.h>
 #include <errno.h>
 
-char **parse_input(char *input){
-  char *mcmd[100]; //array of all commands
+char *parse_line(char *cmdL){
+  char *cmd[512];
   int i = 0;
-  while (input){
-    mcmd[i] = strsep(&input, ";");
+  while (cmdL){
+    cmd[i] = strsep(&cmdL, ";");
     i++;
   }
-  return mcmd;
+  return &cmd;
 }
 
-char **parse_cmd(char *cmdL){
-  char *cmd[100]; //array of single command + args
+char *parse_cmd(char *cmdL){
+  char *cmd[512]; //array of single command + args
   int i = 0;
   while (cmdL){
     cmd[i] = strsep(&cmdL," ");
     i++;
   }
-  cmd[i] = NULL;
-  return cmd;
+  return &cmd;
 }
 
 int main(){
-  char *input[100];
-  fgets(input, sizeof(input), stdin);
-  input[strlen(input)-1] = NULL;
+  char input[512];
+  char *iP = input;
 
-  char **mcmd = parse_input(input);
-  char *cmds[100];
+  fgets(input, sizeof(input), stdin);
+  input[strlen(input)-1] = 0;
+
+  printf("input: %s\n",input);
+
+  char *lines = parse_cmd(input); 
   int i = 0;
-  while(mcmd){
-    cmds[i] = parse_cmd(mcmd[i]);
+  while(lines[i]){
+    printf("%s",lines[i]);
     i++;
   }
-  /*
-  i = 0;
-  while(cmds[0][i]){
-    printf("%s\n",cmds[0][i]);
-    i++;
-    }*/
-
-  //execvp(cmds[0][0],cmds[0]);
   return 0;
 }
