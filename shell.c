ffe0001378a62ebf4d42e182a30be543a34e0f3b
Dorothy Ng <dng1@stuy.edu>
2016-11-28 02:46:38 -0500

    can parse semicolons now woot

--- shell.c
+++ shell.c
@@ -3,40 +3,58 @@
 #include <unistd.h>
 #include <errno.h>
 
-char *parse_line(char *cmdL){
-  char *cmd[512];
-  int i = 0;
-  while (cmdL){
-    cmd[i] = strsep(&cmdL, ";");
-    i++;
-  }
-  return &cmd;
-}
+struct cmdLine parse_cmd(char *cmdL);
+struct cmdArr parse_line(char *cmdL);
 
-char *parse_cmd(char *cmdL){
-  char *cmd[512]; //array of single command + args
-  int i = 0;
-  while (cmdL){
-    cmd[i] = strsep(&cmdL," ");
-    i++;
-  }
-  return &cmd;
-}
+struct cmdLine{
+  char *cmd[512];
+};
+
+struct cmdArr{
+  struct cmdLine arr[512];
+};
 
 int main(){
   char input[512];
   char *iP = input;
 
   fgets(input, sizeof(input), stdin);
-  input[strlen(input)-1] = 0;
+  input[strlen(input)-1] = NULL;
 
   printf("input: %s\n",input);
 
-  char *lines = parse_cmd(input); 
+  struct cmdArr lines = parse_line(input);
+
   int i = 0;
-  while(lines[i]){
-    printf("%s",lines[i]);
-    i++;
+  while(&lines.arr[i].cmd[0]){
+    fork();
+    execvp(lines.arr[i].cmd[0],lines.arr[i].cmd);
   }
   return 0;
+}
+
+//--------------------------------
+
+struct cmdArr parse_line(char *cmdL){
+  struct cmdArr multi;
+  int i = 0;
+  while (cmdL){
+    multi.arr[i] = parse_cmd(strsep(&cmdL, ";"));
+    i++;
+  }
+  struct cmdLine nil;
+  nil.cmd[0] = NULL;
+  multi.arr[i] = nil;
+  return multi;
+}
+
+struct cmdLine parse_cmd(char *cmdL){
+  struct cmdLine single; //array of single command + args
+  int i = 0;
+  while (cmdL){
+    single.cmd[i] = strsep(&cmdL," ");
+    i++;
+   }
+   single.cmd[i] = NULL;
+  return single;
 }
