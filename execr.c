30b9ffd239efb571bf3aa212716ef3528bf5456a
yureistar <dng1@stuy.edu>
2017-01-11 11:45:30 -0500

    tb

--- execr.c
+++ execr.c
@@ -9,36 +9,42 @@
 
 #include "execr.h"
 
-void execRedirO(char **cmd){ // cmd > file
-  char **command = (char **)calloc(10,10);
-  char *file;
-  
-  int ci = 0;
-  int i = 0;
-  int signPassed = 0;
-  while(cmd[i]){
-    if (!signPassed){
-      command[ci] = cmd[i];
-      ci++;
-    }
-    else if (strcmp(cmd[i],">") == 0){
-      signPassed = 1;
-    }
-    else {
-      file = cmd[i];
-    }
-    i++;
+int currentSTD_IN = 0;
+int currentSTD_OUT = 1;
+
+void resetFileTable(in, out) {
+  if (in != 0) {
+    int in_fd = open("STDIN", O_RDONLY);
+    dup2(in_fd, 0);
+    close(in_fd);
   }
-  int filed = open(file, O_WRONLY | O_TRUNC | O_CREAT, 0644);
-  dup2(filed,STDOUT_FILENO);
-  close(filed);
-  execvp(command[0],command);
+  if (out != 1){
+    int out_fd = open("STDOUT", O_RDONLY);
+    dup2(out_fd, 1);
+    close(out_fd);
+  }
 }
 
-void execRedirI(char **cmd){ // cmd < file
+void execRedirO(char **cmd){ // cmd > file
+  char *in = cmd[0];
+  char *out = cmd[2];
+
+  int in_fd = open(in , O_RDONLY);
+  dup2(in_fd , STDIN_FILENO);
+  close(in_fd);
+
+  int out_fd = open(out, O_WRONLY|O_CREAT, 0666);
+  dup2(out_fd , STDOUT_FILENO);
+  close(out_fd);
+
+  execvp(cmd[0], cmd);
+}
+
+// REDIRECTION HERE
+void execRedirI(char **cmd){ // input:"command < file"
   char **command;
   char *file;
-  
+
   int ci = 0;
   int i = 0;
   int signPassed = 0;
@@ -62,8 +68,9 @@ void execRedirI(char **cmd){ // cmd < file
 }
 
 void execPipe(char **cmd){
-  char **command1;
-  char **command2;
+  char command1[50];
+  char command2[50];
+  char tempCmd[30];
 
   int ci = 0;
   int fi = 0;
@@ -83,24 +90,30 @@ void execPipe(char **cmd){
     }
     i++;
   }
-  int out = dup(STDOUT_FILENO);
-  int in = dup(STDIN_FILENO);
-  int fd[2];
-  pipe(fd);
-  int f = fork();
-  int status;
-  if (f == 0){
-    dup2(fd[0],STDOUT_FILENO);
-    close(fd[0]);
-    execvp(command1[0],command1);
-  }
-  else{
-    wait(&status);
-    dup2(fd[1],STDIN_FILENO);
-    close(fd[1]);
-    execvp(command2[2],command2);
+  i = 2;
+  while(i){
+    int f = fork();
+    if (f){
+      wait(0);
+    }
+    else {
+      if (i){
+	strcpy(tempCmd,command1);
+	strcat(tempCmd," > .tmpy");
+	printf("%s",tempCmd);
+      }
+      else {
+	strcpy(tempCmd,command1);
+        strcat(tempCmd," < .tmpy");
+	printf("%s",tempCmd);
+      }
+      exit(0);
+    }
+    execlp("rm","rm",".tmpy",NULL);
   }
 }
+
+//code below works
 
 void execCommand(char **cmd){
   if (strcmp(cmd[0],"exit") == 0){
@@ -115,30 +128,31 @@ void execCommand(char **cmd){
     if (f==0){
       int i = 0;
       while (cmd[i]){
-	if (strcmp(cmd[i],">") == 0){
-	  special = ">";
-	}
-	else if (strcmp(cmd[i],"<") == 0){
-	  special = "<";
-	}
-	else if (strcmp(cmd[i],"|") == 0){
-	  special = "|";
-	}
-	i++;
-      }
-      
-      if (strcmp(special,">") == 0){
-	execRedirO(cmd);
-      }
-      else if (strcmp(special,"<") == 0){
-	execRedirI(cmd);
-      }
-      else if (strcmp(special,"|") == 0){
-	execPipe(cmd);
-      }
-      else{
-	execvp(cmd[0],cmd);
-      }
+	      if (strcmp(cmd[i],">") == 0){
+	          special = ">";
+        }
+	      else if (strcmp(cmd[i],"<") == 0){
+	         special = "<";
+	      }
+	      else if (strcmp(cmd[i],"|") == 0){
+	         special = "|";
+	      }
+	      i++;
+     }
+
+     if (strcmp(special,">") == 0){
+	      execRedirO(cmd);
+     }
+     else if (strcmp(special,"<") == 0){
+	      execRedirI(cmd);
+     }
+     else if (strcmp(special,"|") == 0){
+	      execPipe(cmd);
+     }
+     else{
+	      execvp(cmd[0],cmd);
+     }
+     //resetFileTable(currentSTD_IN, currentSTD_OUT);
     }
     else {
       int status;
